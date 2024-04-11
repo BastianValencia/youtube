4c3ee1f05182c3499e902c735e42258120329164
BastianValencia <79810278+BastianValencia@users.noreply.github.com>
2024-04-11 13:52:52 -0400

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -15,56 +15,5 @@ import 'package:flutter/foundation.dart'
 /// );
 /// ```
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      default:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions are not supported for this platform.',
-        );
-    }
-  }
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCQAGeDxgc_ewu6u60J8i2oeLCD9k2iu_s',
-    appId: '1:752975067008:android:d51b5212e1f7deefc03365',
-    messagingSenderId: '752975067008',
-    projectId: 'push-1dfe3',
-    databaseURL: 'https://push-1dfe3-default-rtdb.firebaseio.com',
-    storageBucket: 'push-1dfe3.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBwGmxghphfurHsE752PGXjfeHtgptBv5k',
-    appId: '1:752975067008:ios:16040fa63bddc28fc03365',
-    messagingSenderId: '752975067008',
-    projectId: 'push-1dfe3',
-    databaseURL: 'https://push-1dfe3-default-rtdb.firebaseio.com',
-    storageBucket: 'push-1dfe3.appspot.com',
-    iosBundleId: 'com.bastianvalencia.pushyoutube',
-  );
+// YOU FIREBASE CONFIG
 }
